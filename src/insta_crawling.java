aeeb95ad7ee2941c2d8d85a88503f6d22dbef876
majayong71 <com4032647@gmail.com>
2022-03-04 19:24:38 +0900

    .select 문법 오류 수정중...(미해결)

--- src/insta_crawling.java
+++ src/insta_crawling.java
@@ -12,15 +12,17 @@ public class insta_crawling {
         {
             String url = "https://www.instagram.com/explore/tags/%ED%99%8D%EB%8C%80%ED%95%AB%ED%94%8C/?hl=ko"; // 크롤링 할 주소
             Document doc = Jsoup.connect(url).get(); // 문서 가져오기.
-            Elements e1 = doc.getElementsByAttributeValue("class","KL4Bh"); // e1 에 class 값이 KL4Bh 인 부분 담기
-            Elements img1 = doc.select("div.KL4Bh");
-            Elements e2 = doc.getElementsByClass("KL4Bh");
-            Elements e3 = doc.getElementsByTag("KL4Bh");
+            Elements e1 = doc.getElementsByAttributeValue("class","FFVAD"); // e1 에 class 값이 KL4Bh 인 부분 담기
+            Elements img1 = doc.select(".FFVAD");
+            Elements e2 = doc.getElementsByClass("FFVAD");
+            Elements e3 = doc.getElementsByTag("FFVAD");
 
             System.out.println(e1);
             System.out.println(img1);
             System.out.println(e2);
             System.out.println(e3);
+
+            System.out.println(img1.attr("src"));
 
 
         }
