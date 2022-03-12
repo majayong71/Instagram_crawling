72987c4f21db8ac7f5d9ccf56b711199210c3af3
majayong71 <com4032647@gmail.com>
2022-03-12 20:06:56 +0900

    로컬 DB 에 img_source 담기 완료

--- src/database.java
+++ src/database.java
@@ -36,39 +36,22 @@ public class database {
 
     public static void main(String[] args) {
 
-        database database = new database();
-
-        database.crawling_data();
-
-
-
         Connection con;
         PreparedStatement pstmt;
 
+        String img1 = "https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/275623616_1116531799135726_9169152130124622986_n.webp?stp=dst-webp_e35&cb=9ad74b5e-95d2b877&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=110&_nc_ohc=LWgvYNxoutcAX9muQaQ&tn=nZuhcPgjqr4tEv-a&edm=ALQROFkBAAAA&ccb=7-4&ig_cache_key=Mjc5MjA4OTk0MTcwMzQzMDM3Mg%3D%3D.2-ccb7-4&oh=00_AT8W0IQzknNAIwD5XYRHLZ69P0kAG1TfkRi32UR1GjrV6w&oe=6232F814&_nc_sid=30a2ef";
+        String img2 = "https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/275518448_695670461614736_3706352208556107491_n.webp?stp=dst-webp_e35&cb=9ad74b5e-95d2b877&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=108&_nc_ohc=T7su0dRV-ZcAX_hnUkK&edm=ABZsPhsBAAAA&ccb=7-4&ig_cache_key=Mjc5MjA4OTQ0NzU4OTI5NjY3NQ%3D%3D.2-ccb7-4&oh=00_AT_vmEHXoqjMJNc5zQSgnyM8kZmlsJK0XGkdWRsGXCsOuQ&oe=62326362&_nc_sid=4efc9f";
+        String img3 = "https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/275515289_127866999788066_6464758431883133110_n.webp?stp=dst-webp_e35&cb=9ad74b5e-95d2b877&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=3w-iBRLRqV4AX8Y-iv-&edm=ABZsPhsBAAAA&ccb=7-4&ig_cache_key=Mjc5MTE4MzgxMTk1NzAwMDQ0OA%3D%3D.2-ccb7-4&oh=00_AT_FPEa4C2xI4VW2RIsecg-WcIreAwOuLomN0pX5H2kuIQ&oe=6232F208&_nc_sid=4efc9f";
+        String img4 = "https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/275515289_127866999788066_6464758431883133110_n.webp?stp=dst-webp_e35&cb=9ad74b5e-95d2b877&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=3w-iBRLRqV4AX8Y-iv-&edm=ABZsPhsBAAAA&ccb=7-4&ig_cache_key=Mjc5MTE4MzgxMTk1NzAwMDQ0OA%3D%3D.2-ccb7-4&oh=00_AT_FPEa4C2xI4VW2RIsecg-WcIreAwOuLomN0pX5H2kuIQ&oe=6232F208&_nc_sid=4efc9f";
+        String img5 = "https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/275226200_527292192347149_2842015571693201285_n.webp?stp=dst-webp_e35&cb=9ad74b5e-95d2b877&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=101&_nc_ohc=ncxbAeS-J1sAX949oRT&tn=nZuhcPgjqr4tEv-a&edm=ABZsPhsBAAAA&ccb=7-4&ig_cache_key=Mjc4ODI5MjkxMzQzNDg4MTMwOQ%3D%3D.2-ccb7-4&oh=00_AT9eMTDm6Hm1ng3ePhkhE5wXt_sGjSJqywgFXq0ssErTVA&oe=6233C219&_nc_sid=4efc9f";
+        String img6 = "https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/275612768_1601637100204637_1164132206439927686_n.jpg?stp=dst-webp_e35&cb=9ad74b5e-95d2b877&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=105&_nc_ohc=z3dsxCPbLMYAX8oKaq5&edm=ABZsPhsBAAAA&ccb=7-4&ig_cache_key=Mjc5MTg4MDQxODQ4NjI4MzQ0NA%3D%3D.2-ccb7-4&oh=00_AT8LT5khjso53ZUCiaevrAGhQ0SH8HuLniwNFQjsnIyJlw&oe=62341C3A&_nc_sid=4efc9f";
+        String img7 = "https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/275599894_507620457693228_2872863216210058671_n.jpg?stp=dst-webp_e35&cb=9ad74b5e-95d2b877&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=100&_nc_ohc=llHhFXt1r40AX93sPqN&edm=ABZsPhsBAAAA&ccb=7-4&ig_cache_key=Mjc5MjAzOTY2NjYxOTkwNDc3MQ%3D%3D.2-ccb7-4&oh=00_AT-qYS-P9ECKQC1hcI-s9b95DbpKVvPY7zv2mZF469GqtA&oe=623350DB&_nc_sid=4efc9f";
+        String img8 = "https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/275541226_4914518948631281_2856873257723146173_n.jpg?stp=dst-webp_e35&cb=9ad74b5e-95d2b877&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=105&_nc_ohc=2UnrRgpIqVMAX89aNQW&edm=ABZsPhsBAAAA&ccb=7-4&ig_cache_key=Mjc5MjAzNjc5NjY2ODEyOTI1Mw%3D%3D.2-ccb7-4&oh=00_AT-mMbTWqSYy415WJZmLwHw2doKep1VSHN7foOd54rI0hA&oe=62341611&_nc_sid=4efc9f";
+        String img9 = "https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/275274400_999330507340628_1696192635965113904_n.webp?stp=dst-webp_e35&cb=9ad74b5e-95d2b877&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=108&_nc_ohc=lNKalh6cBQgAX86-g4d&edm=ABZsPhsBAAAA&ccb=7-4&ig_cache_key=Mjc4Njg2NjQzNTM1MDM1MDU0MQ%3D%3D.2-ccb7-4&oh=00_AT-4Pj4PO-P7D5p5e-mWTwsbQYh3CwkZV-bBq2sWSQt0NQ&oe=62343ADC&_nc_sid=4efc9f";
+
         try {
 
-            try {
-
-                String url = "https://finance.naver.com/item/main.nhn?code=086900";
-
-                Document doc = Jsoup.connect(url).get();
-
-                Elements e1 = doc.getElementsByAttributeValue("class", "no_today");
-                Element e2 = e1.get(0);
-                Elements e3 = e2.select("span");
-
-                result = e3.get(0).text();
-
-                System.out.println(result);
-
-            }
-
-            catch (IOException e)
-            {
-                System.err.println(" 에러가 났습니다 . ");
-            }
-
-            String sql = "INSERT INTO crawling(placeType,timeType,created,count,IMG_SOURCE)values(?,?,?,?,?)";
+            String sql = "INSERT INTO crawling(count,placeType,created,timeType,img_source)values(?,?,now(),?,?)";
 
             Class.forName("com.mysql.cj.jdbc.Driver");
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/nawa", "root", "QhrtnswoTkddl1!6");
@@ -79,12 +62,10 @@ public class database {
 
             pstmt = con.prepareStatement(sql);
 
-            pstmt.setString(1, "1");
-            pstmt.setString(2, "test");
-            pstmt.setString(3, "2022-02-28");
-            pstmt.setInt(4, 0);
-            pstmt.setString(5,result);
-
+            pstmt.setInt(1,0);
+            pstmt.setString(2,"텐동집 (음식점)");
+            pstmt.setString(3,"실시간");
+            pstmt.setString(4,img9);
 
             pstmt.executeUpdate();
 
@@ -93,11 +74,9 @@ public class database {
             pstmt.close();
             con.close();
 
-
         } catch (ClassNotFoundException e) {
             System.err.println(" 에러가 났습니다. (드라이버 로딩 실패) " + e.getMessage());
         } catch (SQLException e) {
-
             System.err.println("에러 발생 . " + e.getMessage());
             e.printStackTrace();
         }
--- src/insta_crawling.java
+++ src/insta_crawling.java
@@ -10,19 +10,29 @@ public class insta_crawling {
 
         try
         {
-            String url = "https://www.instagram.com/explore/tags/%ED%99%8D%EB%8C%80%ED%95%AB%ED%94%8C/?hl=ko"; // 크롤링 할 주소
+            String url = "https://www.instagram.com/explore/tags/%ED%99%8D%EB%8C%80%ED%95%AB%ED%94%8C/?hl=ko";
             Document doc = Jsoup.connect(url).get(); // 문서 가져오기.
-            Elements e1 = doc.getElementsByAttributeValue("class","FFVAD"); // e1 에 class 값이 KL4Bh 인 부분 담기
-            Elements img1 = doc.select(".FFVAD");
-            Elements e2 = doc.getElementsByClass("FFVAD");
-            Elements e3 = doc.getElementsByTag("FFVAD");
 
-            System.out.println(e1);
-            System.out.println(img1);
-            System.out.println(e2);
-            System.out.println(e3);
+            String img1 = "https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/275623616_1116531799135726_9169152130124622986_n.webp?stp=dst-webp_e35&cb=9ad74b5e-95d2b877&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=110&_nc_ohc=LWgvYNxoutcAX9muQaQ&tn=nZuhcPgjqr4tEv-a&edm=ALQROFkBAAAA&ccb=7-4&ig_cache_key=Mjc5MjA4OTk0MTcwMzQzMDM3Mg%3D%3D.2-ccb7-4&oh=00_AT8W0IQzknNAIwD5XYRHLZ69P0kAG1TfkRi32UR1GjrV6w&oe=6232F814&_nc_sid=30a2ef";
+            String img2 = "https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/275518448_695670461614736_3706352208556107491_n.webp?stp=dst-webp_e35&cb=9ad74b5e-95d2b877&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=108&_nc_ohc=T7su0dRV-ZcAX_hnUkK&edm=ABZsPhsBAAAA&ccb=7-4&ig_cache_key=Mjc5MjA4OTQ0NzU4OTI5NjY3NQ%3D%3D.2-ccb7-4&oh=00_AT_vmEHXoqjMJNc5zQSgnyM8kZmlsJK0XGkdWRsGXCsOuQ&oe=62326362&_nc_sid=4efc9f";
+            String img3 = "https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/275515289_127866999788066_6464758431883133110_n.webp?stp=dst-webp_e35&cb=9ad74b5e-95d2b877&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=3w-iBRLRqV4AX8Y-iv-&edm=ABZsPhsBAAAA&ccb=7-4&ig_cache_key=Mjc5MTE4MzgxMTk1NzAwMDQ0OA%3D%3D.2-ccb7-4&oh=00_AT_FPEa4C2xI4VW2RIsecg-WcIreAwOuLomN0pX5H2kuIQ&oe=6232F208&_nc_sid=4efc9f";
+            String img4 = "https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/275515289_127866999788066_6464758431883133110_n.webp?stp=dst-webp_e35&cb=9ad74b5e-95d2b877&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=3w-iBRLRqV4AX8Y-iv-&edm=ABZsPhsBAAAA&ccb=7-4&ig_cache_key=Mjc5MTE4MzgxMTk1NzAwMDQ0OA%3D%3D.2-ccb7-4&oh=00_AT_FPEa4C2xI4VW2RIsecg-WcIreAwOuLomN0pX5H2kuIQ&oe=6232F208&_nc_sid=4efc9f";
+            String img5 = "https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/275226200_527292192347149_2842015571693201285_n.webp?stp=dst-webp_e35&cb=9ad74b5e-95d2b877&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=101&_nc_ohc=ncxbAeS-J1sAX949oRT&tn=nZuhcPgjqr4tEv-a&edm=ABZsPhsBAAAA&ccb=7-4&ig_cache_key=Mjc4ODI5MjkxMzQzNDg4MTMwOQ%3D%3D.2-ccb7-4&oh=00_AT9eMTDm6Hm1ng3ePhkhE5wXt_sGjSJqywgFXq0ssErTVA&oe=6233C219&_nc_sid=4efc9f";
+            String img6 = "https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/275612768_1601637100204637_1164132206439927686_n.jpg?stp=dst-webp_e35&cb=9ad74b5e-95d2b877&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=105&_nc_ohc=z3dsxCPbLMYAX8oKaq5&edm=ABZsPhsBAAAA&ccb=7-4&ig_cache_key=Mjc5MTg4MDQxODQ4NjI4MzQ0NA%3D%3D.2-ccb7-4&oh=00_AT8LT5khjso53ZUCiaevrAGhQ0SH8HuLniwNFQjsnIyJlw&oe=62341C3A&_nc_sid=4efc9f";
+            String img7 = "https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/275599894_507620457693228_2872863216210058671_n.jpg?stp=dst-webp_e35&cb=9ad74b5e-95d2b877&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=100&_nc_ohc=llHhFXt1r40AX93sPqN&edm=ABZsPhsBAAAA&ccb=7-4&ig_cache_key=Mjc5MjAzOTY2NjYxOTkwNDc3MQ%3D%3D.2-ccb7-4&oh=00_AT-qYS-P9ECKQC1hcI-s9b95DbpKVvPY7zv2mZF469GqtA&oe=623350DB&_nc_sid=4efc9f";
+            String img8 = "https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/275541226_4914518948631281_2856873257723146173_n.jpg?stp=dst-webp_e35&cb=9ad74b5e-95d2b877&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=105&_nc_ohc=2UnrRgpIqVMAX89aNQW&edm=ABZsPhsBAAAA&ccb=7-4&ig_cache_key=Mjc5MjAzNjc5NjY2ODEyOTI1Mw%3D%3D.2-ccb7-4&oh=00_AT-mMbTWqSYy415WJZmLwHw2doKep1VSHN7foOd54rI0hA&oe=62341611&_nc_sid=4efc9f";
+            String img9 = "https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/275274400_999330507340628_1696192635965113904_n.webp?stp=dst-webp_e35&cb=9ad74b5e-95d2b877&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=108&_nc_ohc=lNKalh6cBQgAX86-g4d&edm=ABZsPhsBAAAA&ccb=7-4&ig_cache_key=Mjc4Njg2NjQzNTM1MDM1MDU0MQ%3D%3D.2-ccb7-4&oh=00_AT-4Pj4PO-P7D5p5e-mWTwsbQYh3CwkZV-bBq2sWSQt0NQ&oe=62343ADC&_nc_sid=4efc9f";
 
-            System.out.println(img1.attr("src"));
+//            Elements e1 = doc.getElementsByAttributeValue("class","FFVAD"); // e1 에 class 값이 KL4Bh 인 부분 담기
+//            Elements img1 = doc.select("FFVAD");
+//            Elements e2 = doc.getElementsByClass("FFVAD");
+//            Elements e3 = doc.getElementsByTag("FFVAD");
+//
+//            Elements e11 = doc.getElementsByAttributeValue("class","KL4Bh"); // e1 에 class 값이 KL4Bh 인 부분 담기
+//            Elements img11 = doc.select(".KL4Bh");
+//            Elements e22 = doc.getElementsByClass(".KL4Bh");
+//            Elements e33 = doc.getElementsByTag(".KL4Bh");
+
 
 
         }
@@ -34,31 +44,5 @@ public class insta_crawling {
 
     }
 }
-
-//
-//        try {
-//
-//                String url = "https://finance.naver.com/item/main.nhn?code=086900";
-//
-//                Document doc = Jsoup.connect(url).get();
-//
-//                Elements e1 = doc.getElementsByAttributeValue("class", "no_today");
-//                Element e2 = e1.get(0);
-//                Elements e3 = e2.select("span");
-//
-//                String result = e3.get(0).text();
-//
-//                System.out.println(result);
-//
-//                }
-//
-//                catch (IOException e)
-//                {
-//                System.out.println("에러가 생겼습니다"+e.getMessage());
-//                }
-//
-//
-
-
 
 // FFVAD KL4Bh
